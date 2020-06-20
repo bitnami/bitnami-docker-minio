5ca8ce7a59d0d753d4d8247f992d9bc189ecd5d7
adnoh <github@rotzefull.de>
2020-06-20 18:27:50 +0300

    Enable to specify default bucket policy for MINIO_DEFAULT_BUCKETS
    Enables you to specify a policy for the newly created MINIO_DEFAULT_BUCKETS like:
    MINIO_DEFAULT_BUCKETS=mynewpubrw:public,mynewro:download,normalbucket,.....

--- 2020/debian-10/rootfs/opt/bitnami/scripts/libminio.sh
+++ 2020/debian-10/rootfs/opt/bitnami/scripts/libminio.sh
@@ -228,14 +228,19 @@ minio_create_default_buckets() {
         read -r -a buckets <<< "$(tr ',;' ' ' <<< "${MINIO_DEFAULT_BUCKETS}")"
         info "Creating default buckets..."
         for b in "${buckets[@]}"; do
+		    read -r -a Options <<< "$(tr ':' ' ' <<< "${b}")"
             if ! minio_client_bucket_exists "local/${b}"; then
                 if [[ -n "${MINIO_REGION_NAME:-}" ]]; then
-                    minio_client_execute mb "--region" "${MINIO_REGION_NAME}" "local/${b}"
+                    minio_client_execute mb "--region" "${MINIO_REGION_NAME}" "local/${Options[0]}"
                 else
-                    minio_client_execute mb "local/${b}"
+                    minio_client_execute mb "local/${Options[0]}"
+                fi
+                if [ ${#Options[@]} -eq 2 ]; then
+                    info "Setting policy ${Options[1]} for local bucket ${Options[0]}"
+                    minio_client_execute policy set "${Options[1]}" local/"${Options[0]}"/
                 fi
             else
-                info "Bucket local/${b} already exists, skipping creation."
+                info "Bucket local/${Options[0]} already exists, skipping creation."
             fi
         done
     fi
